eca58a9bf4dfa2d5d478a0cf1a73fb6a71c21a49
Kelly de Vries <kdv@me.com>
2015-04-17 09:39:10 -0700

    pass tests for small and large pepperoni pizzas.

--- spec/specs.js
+++ spec/specs.js
@@ -7,4 +7,20 @@ describe('Pizza', function(){
             expect(testPizza.getPrice()).to.equal(10);
         });
     });
+    describe('getPrice', function(){
+        it("will return '12' for the price of a small pepperoni pizza", function(){
+            var testPizza = Object.create(Pizza);
+            testPizza.type = 1;
+            testPizza.size = 0;
+            expect(testPizza.getPrice()).to.equal(12);
+        });
+    });
+    describe('getPrice', function(){
+        it("will return '14' for the price of a large pepperoni pizza", function(){
+            var testPizza = Object.create(Pizza);
+            testPizza.type = 1;
+            testPizza.size = 1;
+            expect(testPizza.getPrice()).to.equal(14);
+        });
+    });
 });
