929c1e506b1178264ed41aa77d1bba6865074bc4
Kelly de Vries <kdv@me.com>
2015-04-17 10:12:52 -0700

    add functioning html and JS to receive order info and display result to customer. still needs hide/show functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,31 @@
 var Pizza = {
-    init: function(){
-        this.type = [];
-        this.size = [];
-    },
-    getPrice: function(){
-        return 10 + (this.type * 2)
-                  + (this.size * 2);
-    }
+  init: function(){
+      this.type = [];
+      this.size = [];
+  },
+  getPrice: function(){
+      return 10 + parseInt(this.type * 2)
+                + parseInt(this.size * 2);
+  }
 };
+
+$(document).ready(function(){
+  $("form#order_form").submit(function(event){
+    event.preventDefault();
+
+    var newPizza = Object.create(Pizza);
+    newPizza.init();
+    newPizza.type = $("select#pizza_type").val();
+    console.log(newPizza.type);
+    newPizza.size = $("select#pizza_size").val();
+    console.log(newPizza.type);
+    var pizzaCost = newPizza.getPrice();
+    console.log(pizzaCost);
+
+    $("select#pizza_type").val("");
+    $("select#pizza_size").val("");
+
+    $(".pizza_cost").text(pizzaCost);
+
+  });
+});
